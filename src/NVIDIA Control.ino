53b50e678eb03b428289cdc89c7417a07f6879cc
hannemann <hannemann666@gmx.net>
2021-12-08 18:30:07 +0100

    first tweaks
    * add platformio config
    * change keyboard definition
    * fix warnings
    * change possible keys
    * implement longpress

--- src/NVIDIA Control.ino
+++ src/NVIDIA Control.ino
@@ -1,6 +1,6 @@
 //Your wifi connection
-const char* ssid = "ssid";
-const char* password = "password";
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 /*Change the port to a random (free) port for security.*/
 const int webSocketPort = 2339;
 
@@ -27,6 +27,7 @@ struct JSONMethodToCecType {
   char JSONMethod[24];
   char JSONAction[14];
   bool KeyboardAction;
+  bool LongPress;
   uint8_t USBHID;
 };
 
@@ -40,32 +41,31 @@ IPAddressFail IPAddressFailures[254];
 
 /*  JsonToUSB_HID_Map */
 const JSONMethodToCecType JSONMethodToCec[] = {
-  {"Input.Select", "", 1, 0x28},
-  {"Input.Left", "", 1, 0x50},
-  {"Input.Right", "", 1, 0x4F},
-  {"Input.Up", "", 1, 0x52},
-  {"Input.Down", "", 1, 0x51},
-  {"Input.ShowOSD", "", 1, 0x10},
-  {"Input.Info", "", 0, 0},
-  {"Input.Back", "", 1, 0x29},
-  {"Input.Home", "", 1, 0x29},
-  {"Input.SendText", "", 0, 0},
-  {"VideoLibrary.Scan", "", 0, 0},
-  {"Input.ContextMenu", "", 0, 0},
-  {"Player.GetActivePlayers", "", 0, 0},
-  /* Pause - the shield does not require this, but a normal PC or perhaps other Android TV do*/
-  {"Player.PlayPause", "1", 0, 0xB1},
-  /* Play */
-  {"Player.PlayPause", "2", 0, 0xB0},
-  {"Player.Stop", "", 0, 0xB7},
-  {"Input.ExecuteAction", "play", 0, 0xB0},
-  {"Input.ExecuteAction", "mute", 0, 0xE2},
-  {"Input.ExecuteAction", "stepback", 0, 0xB4},
-  {"Input.ExecuteAction", "stepforward", 0, 0xB3},
-  {"Input.ExecuteAction", "skipprevious", 0, 0xB7},
-  {"Input.ExecuteAction", "skipnext", 0, 0xB5},
-  {"Input.ExecuteAction", "volumeup", 0, 0xE9},
-  {"Input.ExecuteAction", "volumedown", 0, 0xEA}
+  {"Input.Select",                  "",                 1, 0, 0x28},
+  {"Input.SelectLongpress",         "",                 1, 1, 0x28},
+  {"Input.Left",                    "",                 1, 0, 0x50},
+  {"Input.Right",                   "",                 1, 0, 0x4F},
+  {"Input.Up",                      "",                 1, 0, 0x52},
+  {"Input.Down",                    "",                 1, 0, 0x51},
+  {"Input.ShowOSD",                 "",                 1, 0, 0x10},
+  {"Input.Info",                    "",                 0, 0, 0},
+  {"Input.Back",                    "",                 1, 0, 0x29},
+  {"Input.Home",                    "",                 1, 0, 0x29},
+
+  {"Input.SendText",                "",                 0, 0, 0},
+  {"VideoLibrary.Scan",             "",                 0, 0, 0},
+  {"Input.ContextMenu",             "",                 1, 0, 0x65},
+  {"Input.ExecuteAction",           "volumeup",         0, 0, 0xE9},
+  {"Input.ExecuteAction",           "volumedown",       0, 0, 0xEA},
+  {"Input.ExecuteAction",           "mute",             0, 0, 0xE2},
+  {"Input.ExecuteAction",           "appswitch",        0, 0, 0xBB},
+  {"Input.ExecuteAction",           "play",             0, 0, 0xCD},
+  {"Input.ExecuteAction",           "stop",             0, 0, 0xB7},
+  {"Input.ExecuteAction",           "stepback",         0, 0, 0xB4},
+
+  {"Input.ExecuteAction",           "stepforward",      0, 0, 0xB3},
+  {"Input.ExecuteAction",           "skipprevious",     0, 0, 0xB7},
+  {"Input.ExecuteAction",           "skipnext",         0, 0, 0xB5}
 };
 
 //Keyboard Initialize
@@ -95,41 +95,76 @@ struct inputKeyboard_t
   uint8_t  Key;                                 // Value = 0 to 101
 };
 
+// Report IDs:
+#define KEYBOARD_ID 0x01
+#define MEDIA_KEYS_ID 0x02
+
 const uint8_t keyboardHidDescriptor[] = {
-  0x05, 0x0c,                    // USAGE_PAGE (Consumer Devices)
-  0x09, 0x01,                    // USAGE (Consumer Control)
-  0xa1, 0x01,                    // COLLECTION (Application)
-  0x85, 0x01,                    //   REPORT_ID (1)
-  0x19, 0x00,                    //   USAGE_MINIMUM (Unassigned)
-  0x2a, 0x3c, 0x02,              //   USAGE_MAXIMUM (AC Format)
-  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-  0x26, 0x3c, 0x02,              //   LOGICAL_MAXIMUM (572)
-  0x95, 0x01,                    //   REPORT_COUNT (1)
-  0x75, 0x10,                    //   REPORT_SIZE (16)
-  0x81, 0x00,                    //   INPUT (Data,Var,Abs)
-  0xc0,                          // END_COLLECTION
-  0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-  0x09, 0x06,                    // USAGE (Keyboard)
-  0xa1, 0x01,                    // COLLECTION (Application)
-  0x85, 0x02,                    //   REPORT_ID (2)
-  0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-  0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-  0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-  0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-  0x75, 0x01,                    //   REPORT_SIZE (1)
-  0x95, 0x08,                    //   REPORT_COUNT (8)
-  0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-  0x95, 0x01,                    //   REPORT_COUNT (1)
-  0x75, 0x08,                    //   REPORT_SIZE (8)
-  0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-  0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-  0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-  0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-  0xc0                           // END_COLLECTION
+  USAGE_PAGE(1),      0x01,          // USAGE_PAGE (Generic Desktop Ctrls)
+  USAGE(1),           0x06,          // USAGE (Keyboard)
+  COLLECTION(1),      0x01,          // COLLECTION (Application)
+  // ------------------------------------------------- Keyboard
+  REPORT_ID(1),       KEYBOARD_ID,   //   REPORT_ID (1)
+  USAGE_PAGE(1),      0x07,          //   USAGE_PAGE (Kbrd/Keypad)
+  USAGE_MINIMUM(1),   0xE0,          //   USAGE_MINIMUM (0xE0)
+  USAGE_MAXIMUM(1),   0xE7,          //   USAGE_MAXIMUM (0xE7)
+  LOGICAL_MINIMUM(1), 0x00,          //   LOGICAL_MINIMUM (0)
+  LOGICAL_MAXIMUM(1), 0x01,          //   Logical Maximum (1)
+  REPORT_SIZE(1),     0x01,          //   REPORT_SIZE (1)
+  REPORT_COUNT(1),    0x08,          //   REPORT_COUNT (8)
+  HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  REPORT_COUNT(1),    0x01,          //   REPORT_COUNT (1) ; 1 byte (Reserved)
+  REPORT_SIZE(1),     0x08,          //   REPORT_SIZE (8)
+  HIDINPUT(1),        0x01,          //   INPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  REPORT_COUNT(1),    0x05,          //   REPORT_COUNT (5) ; 5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
+  REPORT_SIZE(1),     0x01,          //   REPORT_SIZE (1)
+  USAGE_PAGE(1),      0x08,          //   USAGE_PAGE (LEDs)
+  USAGE_MINIMUM(1),   0x01,          //   USAGE_MINIMUM (0x01) ; Num Lock
+  USAGE_MAXIMUM(1),   0x05,          //   USAGE_MAXIMUM (0x05) ; Kana
+  HIDOUTPUT(1),       0x02,          //   OUTPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  REPORT_COUNT(1),    0x01,          //   REPORT_COUNT (1) ; 3 bits (Padding)
+  REPORT_SIZE(1),     0x03,          //   REPORT_SIZE (3)
+  HIDOUTPUT(1),       0x01,          //   OUTPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  REPORT_COUNT(1),    0x06,          //   REPORT_COUNT (6) ; 6 bytes (Keys)
+  REPORT_SIZE(1),     0x08,          //   REPORT_SIZE(8)
+  LOGICAL_MINIMUM(1), 0x00,          //   LOGICAL_MINIMUM(0)
+  LOGICAL_MAXIMUM(1), 0x65,          //   LOGICAL_MAXIMUM(0x65) ; 101 keys
+  USAGE_PAGE(1),      0x07,          //   USAGE_PAGE (Kbrd/Keypad)
+  USAGE_MINIMUM(1),   0x00,          //   USAGE_MINIMUM (0)
+  USAGE_MAXIMUM(1),   0x65,          //   USAGE_MAXIMUM (0x65)
+  HIDINPUT(1),        0x00,          //   INPUT (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  END_COLLECTION(0),                 // END_COLLECTION
+  // ------------------------------------------------- Media Keys
+  USAGE_PAGE(1),      0x0C,          // USAGE_PAGE (Consumer)
+  USAGE(1),           0x01,          // USAGE (Consumer Control)
+  COLLECTION(1),      0x01,          // COLLECTION (Application)
+  REPORT_ID(1),       MEDIA_KEYS_ID, //   REPORT_ID (3)
+  USAGE_PAGE(1),      0x0C,          //   USAGE_PAGE (Consumer)
+  LOGICAL_MINIMUM(1), 0x00,          //   LOGICAL_MINIMUM (0)
+  LOGICAL_MAXIMUM(1), 0x01,          //   LOGICAL_MAXIMUM (1)
+  REPORT_SIZE(1),     0x01,          //   REPORT_SIZE (1)
+  REPORT_COUNT(1),    0x10,          //   REPORT_COUNT (16)
+  USAGE(1),           0xB5,          //   USAGE (Scan Next Track)     ; bit 0: 1
+  USAGE(1),           0xB6,          //   USAGE (Scan Previous Track) ; bit 1: 2
+  USAGE(1),           0xB7,          //   USAGE (Stop)                ; bit 2: 4
+  USAGE(1),           0xCD,          //   USAGE (Play/Pause)          ; bit 3: 8
+  USAGE(1),           0xE2,          //   USAGE (Mute)                ; bit 4: 16
+  USAGE(1),           0xE9,          //   USAGE (Volume Increment)    ; bit 5: 32
+  USAGE(1),           0xEA,          //   USAGE (Volume Decrement)    ; bit 6: 64
+  USAGE(2),           0x23, 0x02,    //   Usage (WWW Home)            ; bit 7: 128
+  USAGE(2),           0x94, 0x01,    //   Usage (My Computer) ; bit 0: 1
+  USAGE(2),           0x92, 0x01,    //   Usage (Calculator)  ; bit 1: 2
+  USAGE(2),           0x2A, 0x02,    //   Usage (WWW fav)     ; bit 2: 4
+  USAGE(2),           0x21, 0x02,    //   Usage (WWW search)  ; bit 3: 8
+  USAGE(2),           0x26, 0x02,    //   Usage (WWW stop)    ; bit 4: 16
+  USAGE(2),           0x24, 0x02,    //   Usage (WWW back)    ; bit 5: 32
+  USAGE(2),           0x83, 0x01,    //   Usage (Media sel)   ; bit 6: 64
+  USAGE(2),           0x8A, 0x01,    //   Usage (Mail)        ; bit 7: 128
+  HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  END_COLLECTION(0)                  // END_COLLECTION
 };
 
-static inputConsumer_t consumer_Report{};
+// static inputConsumer_t consumer_Report{};
 static inputKeyboard_t keyboard_report{}; // sent to PC
 BLEHIDDevice* hid;
 BLECharacteristic* input;
@@ -138,7 +173,7 @@ BLECharacteristic* inputMedia;
 BLECharacteristic* outputMedia;
 bool BLEconnected = false;
 
-void keyboard_control(uint8_t Key, const char* JSONMethod) {
+void keyboard_control(uint8_t Key, const char* JSONMethod, bool longpress) {
   Serial.print("Sending key code :");
   Serial.println(Key);
   inputKeyboard_t keyboard{};
@@ -148,6 +183,13 @@ void keyboard_control(uint8_t Key, const char* JSONMethod) {
   keyboard.Key = Key;
   input->setValue((uint8_t*)&keyboard, sizeof(keyboard));
   input->notify();
+  if (longpress) {
+    for(int i = 0; i < 30; i++) {
+      delay(10);
+      input->setValue((uint8_t*)&keyboard, sizeof(keyboard));
+      input->notify();
+    }
+  }
   input->setValue((uint8_t*)(&keyboard_report), sizeof(keyboard_report));
   input->notify();
   delay(10);
@@ -166,19 +208,22 @@ void media_control(uint8_t Key) {
 }
 
 void processUSBHID(JSONVar JSONMethod, const char* JSONAction) {
-  int JSONMethodToCecLength = 24;
+  int JSONMethodToCecLength = 23;
+  Serial.printf("Incoming JSON Request: %s / %s\n", (const char *)JSONMethod, (const char *)JSONAction);
+  // Serial.printf("Found %d possible candidates\n", (int)JSONMethodToCecLength);
   for (int i = 0; i < JSONMethodToCecLength; i++) {
-    if ((strcmp(JSONMethodToCec[i].JSONMethod, (const char *)JSONMethod) == 0) && (strcmp(JSONMethodToCec[i].JSONAction, (const char *)JSONAction) == 0)) {
-      Serial.print("Incoming JSON Request :");
-      Serial.println((const char *)JSONMethod);
+    // Serial.printf("Candidate: %s / %s\n", JSONMethodToCec[i].JSONMethod, JSONMethodToCec[i].JSONAction);
+    if ((strcmp(JSONMethodToCec[i].JSONMethod, (const char *)JSONMethod) == 0) && (strcmp(JSONMethodToCec[i].JSONAction, JSONAction) == 0)) {
+      Serial.println("JSON valid");
       if (JSONMethodToCec[i].KeyboardAction == 1) {
-        keyboard_control(JSONMethodToCec[i].USBHID, (const char *)JSONMethod);
+        keyboard_control(JSONMethodToCec[i].USBHID, (const char *)JSONMethod, JSONMethodToCec[i].LongPress);
       } else {
         media_control(JSONMethodToCec[i].USBHID);
       }
       return;
     }
   }
+  Serial.println("JSON invalid");
 }
 
 class BLECallback : public BLEServerCallbacks {
@@ -279,7 +324,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
         unsigned long startTime;
         startTime = millis();
 
-        Serial.printf("WEBSOCKET: [%u] get Text: %s\n", num, payload);
+        Serial.printf("WEBSOCKET: [%u] Payload: %s\n", num, payload);
         Serial.printf("Counter: %d\n",counter);
         
         counter = counter + 1;
@@ -309,27 +354,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
           return;
         }
         Serial.println("Correctly parsed JSON");
-        if (strcmp(myObject["method"], "Player.PlayPause") == 0) {
-          /* Leaving this here, it may be essential for a PC or another Android TV Device 
-          if (playing==true) {
-            processUSBHID(myObject["method"], "1");
-            playing = false;
-          } else {
-            processUSBHID(myObject["method"], "2");            
-            playing = true;
-          }          
-          */
-          processUSBHID(myObject["method"], "2");            
-        } else if (strcmp(myObject["method"], "Input.ExecuteAction") == 0) {
+        if (strcmp(myObject["method"], "Input.ExecuteAction") == 0) {
           if (!((myObject.hasOwnProperty("params")) && (myObject["params"].hasOwnProperty("action")))) {
-            Serial.println("JSON parse cannot find params or anction and is required for Input.ExecuteAction");
+            Serial.println("JSON parse cannot find params or action and is required for Input.ExecuteAction");
             return;
           }
           processUSBHID(myObject["method"], myObject["params"]["action"]);
         } else {
           processUSBHID(myObject["method"], "");
         }
-        Serial.printf("Function time was %d\n",millis() - startTime);
+        Serial.printf("Function time was %d\n", (int)(millis() - startTime));
         if (strcmp(myObject["method"], "Player.Stop")==0) {
           /* As the current paying media is stopped, assuming next media state is playing */
           playing = true;
@@ -342,6 +376,8 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
     case WStype_FRAGMENT_BIN_START:
     case WStype_FRAGMENT:
     case WStype_FRAGMENT_FIN:
+    case WStype_PING:
+    case WStype_PONG:
       break;
   }
 
