8658360c96100375e6573a0c5ab22a5fbf857cb8
hannemann <hannemann666@gmx.net>
2021-12-09 22:05:50 +0100

    configurable descriptor aternative

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -22,6 +22,7 @@ struct inputKeyboard_t
   uint8_t  Key;                                 // Value = 0 to 101
 };
 
+#ifdef ALTERNATIVE_KB_TYPE
 // Report IDs:
 #define KEYBOARD_ID 0x01
 #define MEDIA_KEYS_ID 0x02
@@ -90,6 +91,44 @@ const uint8_t keyboardHidDescriptor[] = {
   HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0)                  // END_COLLECTION
 };
+
+#else
+
+const uint8_t keyboardHidDescriptor[] = {
+  0x05, 0x0c,                    // USAGE_PAGE (Consumer Devices)
+  0x09, 0x01,                    // USAGE (Consumer Control)
+  0xa1, 0x01,                    // COLLECTION (Application)
+  0x85, 0x01,                    //   REPORT_ID (1)
+  0x19, 0x00,                    //   USAGE_MINIMUM (Unassigned)
+  0x2a, 0x3c, 0x02,              //   USAGE_MAXIMUM (AC Format)
+  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+  0x26, 0x3c, 0x02,              //   LOGICAL_MAXIMUM (572)
+  0x95, 0x01,                    //   REPORT_COUNT (1)
+  0x75, 0x10,                    //   REPORT_SIZE (16)
+  0x81, 0x00,                    //   INPUT (Data,Var,Abs)
+  0xc0,                          // END_COLLECTION
+  0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+  0x09, 0x06,                    // USAGE (Keyboard)
+  0xa1, 0x01,                    // COLLECTION (Application)
+  0x85, 0x02,                    //   REPORT_ID (2)
+  0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+  0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+  0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+  0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+  0x75, 0x01,                    //   REPORT_SIZE (1)
+  0x95, 0x08,                    //   REPORT_COUNT (8)
+  0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+  0x95, 0x01,                    //   REPORT_COUNT (1)
+  0x75, 0x08,                    //   REPORT_SIZE (8)
+  0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+  0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+  0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+  0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+  0xc0                           // END_COLLECTION
+};
+
+#endif
 
 // static inputConsumer_t consumer_Report{};
 static inputKeyboard_t keyboard_report{}; // sent to PC
