0ad86b412bc41f2a5027ce891c6125c8a6f84dfe
hannemann <hannemann666@gmx.net>
2021-12-13 19:23:46 +0100

    fix: prevent to rapid keystrokes in frontend which can cause wifi to crash

--- data/scripts.js
+++ data/scripts.js
@@ -1,10 +1,12 @@
 var ws = new WebSocket("ws://192.168.178.218:2339/jsonrpc");
+var blocked = false;
 ws.onopen = (e) => document.body.classList.remove("ws-off");
 ws.onerror = (e) => document.body.classList.add("ws-off");
 ws.onclose = (e) => document.body.classList.add("ws-off");
 document.querySelectorAll("button[data-key]").forEach((b) => {
   b.addEventListener("pointerdown", (e) => {
-    if (document.querySelector('[name="learn"]:checked')) return;
+    if (document.querySelector('[name="learn"]:checked') || blocked) return;
+    blocked = true;
     ws.send(
       JSON.stringify({
         method: "keydown",
@@ -15,6 +17,7 @@ document.querySelectorAll("button[data-key]").forEach((b) => {
 });
 document.querySelectorAll("button").forEach((b) => {
   b.addEventListener("pointerup", (e) => {
+    ws.addEventListener("message", (e) => (blocked = false), { once: true });
     if (document.querySelector('[name="learn"]:checked')) {
       ws.send(
         JSON.stringify({
