3eacf1296031fd9f1b3b72021eb37393a07a8ee6
hannemann <hannemann666@gmx.net>
2022-01-04 19:51:48 +0100

    chore: cleanup decriptor comments

--- src/Bluetooth.h
+++ src/Bluetooth.h
@@ -41,13 +41,13 @@ const uint8_t HidDescriptor[] = {
     0xc0,             // END_COLLECTION
 
     /*** MOUSE REPORT ***/
-    USAGE_PAGE(1), 0x01, // USAGE_PAGE (Generic Desktop)
-    USAGE(1), 0x02,      // USAGE (Mouse)
-    COLLECTION(1), 0x01, // COLLECTION (Application)
-    USAGE(1), 0x01,      //   USAGE (Pointer)
-    COLLECTION(1), 0x00, //   COLLECTION (Physical)
-    REPORT_ID(1), 0x03,  //     REPORT_ID (1)
-    // ------------------------------------------------- Buttons (Left, Right, Middle, Back, Forward)
+    USAGE_PAGE(1), 0x01,      // USAGE_PAGE (Generic Desktop)
+    USAGE(1), 0x02,           // USAGE (Mouse)
+    COLLECTION(1), 0x01,      // COLLECTION (Application)
+    USAGE(1), 0x01,           //   USAGE (Pointer)
+    COLLECTION(1), 0x00,      //   COLLECTION (Physical)
+    REPORT_ID(1), 0x03,       //     REPORT_ID (1)
+                              // -------------- Buttons (Left, Right, Middle, Back, Forward)
     USAGE_PAGE(1), 0x09,      //     USAGE_PAGE (Button)
     USAGE_MINIMUM(1), 0x01,   //     USAGE_MINIMUM (Button 1)
     USAGE_MAXIMUM(1), 0x05,   //     USAGE_MAXIMUM (Button 5)
@@ -56,11 +56,11 @@ const uint8_t HidDescriptor[] = {
     REPORT_SIZE(1), 0x01,     //     REPORT_SIZE (1)
     REPORT_COUNT(1), 0x05,    //     REPORT_COUNT (5)
     HIDINPUT(1), 0x02,        //     INPUT (Data, Variable, Absolute) ;5 button bits
-    // ------------------------------------------------- Padding
-    REPORT_SIZE(1), 0x03,  //     REPORT_SIZE (3)
-    REPORT_COUNT(1), 0x01, //     REPORT_COUNT (1)
-    HIDINPUT(1), 0x03,     //     INPUT (Constant, Variable, Absolute) ;3 bit padding
-    // ------------------------------------------------- X/Y position, Wheel
+                              // -------------- Padding
+    REPORT_SIZE(1), 0x03,     //     REPORT_SIZE (3)
+    REPORT_COUNT(1), 0x01,    //     REPORT_COUNT (1)
+    HIDINPUT(1), 0x03,        //     INPUT (Constant, Variable, Absolute) ;3 bit padding
+                              // -------------- X/Y position, Wheel
     USAGE_PAGE(1), 0x01,      //     USAGE_PAGE (Generic Desktop)
     USAGE(1), 0x30,           //     USAGE (X)
     USAGE(1), 0x31,           //     USAGE (Y)
@@ -70,7 +70,7 @@ const uint8_t HidDescriptor[] = {
     REPORT_SIZE(1), 0x08,     //     REPORT_SIZE (8)
     REPORT_COUNT(1), 0x03,    //     REPORT_COUNT (3)
     HIDINPUT(1), 0x06,        //     INPUT (Data, Variable, Relative) ;3 bytes (X,Y,Wheel)
-    // ------------------------------------------------- Horizontal wheel
+                              // -------------- Horizontal wheel
     USAGE_PAGE(1), 0x0c,      //     USAGE PAGE (Consumer Devices)
     USAGE(2), 0x38, 0x02,     //     USAGE (AC Pan)
     LOGICAL_MINIMUM(1), 0x81, //     LOGICAL_MINIMUM (-127)
